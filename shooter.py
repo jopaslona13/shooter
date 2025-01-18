8a74f1b2272e4973fa98e5f919606a30f0aa98ba
Anton <anton.bahnak@gmail.com>
2025-01-18 11:45:09 +0200

    Add files via upload

--- shooter.py
+++ shooter.py
@@ -7,14 +7,22 @@ mixer.music.play()
 fire_sound = mixer.Sound('shoot_sound.ogg')
 
 font.init()
+font1 = font.Font(None, 80)
 font2 = font.Font(None, 36)
+win = font1.render("YOU WIN!", True, (255,255,255))
+lose = font1.render("YOU LOSE!", True, (180,0,0))
 
 img_back = 'background_street.png'
 img_hero = 'guy.png'
 img_enemy = 'zombie.png'
+img_bullet = 'bullet.png'
 
 clock = time.Clock()
 FPS = 60
+
+score = 0
+lost = 0
+max_lost = 3
 
 score = 0
 lost = 0
@@ -40,7 +48,12 @@ class GameSprite(sprite.Sprite):
         
     def reset(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
- 
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
 # клас головного гравця
 class Player(GameSprite):
  
@@ -51,10 +64,13 @@ class Player(GameSprite):
             self.rect.x -= self.speed
         if keys[K_RIGHT] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
- 
-    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
+            
     def fire(self):
-        pass
+        bullet = Bullet(
+            img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15
+        )
+        bullets.add(bullet)
+    # метод "постріл" (використовуємо місце гравця, щоб створити там кулю)
 
 class Enemy(GameSprite):
     def update(self):
@@ -81,6 +97,9 @@ for i in range(1, 6):
     monster = Enemy(img_enemy, randint(
         80, win_width - 80), -40, 120, 120, randint(1, 3))
     monsters.add(monster)
+    
+bullets = sprite.Group()
+
 # змінна "гра закінчилася": як тільки вона стає True, в основному циклі перестають працювати спрайти
 finish = False
  
@@ -92,7 +111,12 @@ while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
- 
+
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+    
     if not finish:
         # оновлюємо фон
         window.blit(background, (0, 0))
@@ -106,10 +130,12 @@ while run:
         # рухи спрайтів
         ship.update()
         monsters.update()
+        bullets.update()
  
         # оновлюємо їх у новому місці при кожній ітерації циклу
         ship.reset()
         monsters.draw(window)
+        bullets.draw(window)
  
     display.update()
     clock.tick(FPS)
